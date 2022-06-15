ff6ada6b5d28e165e57a4d6b5948a5ab47c202e3
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-06-15 09:24:16 -0700

    Betty is proud commit

--- shell.c
+++ shell.c
@@ -2,9 +2,9 @@
 
 /**
  * main - Entry point of the program.
- * @ac: The number of parameters passed to the executable file. In the case
- * this variable will not be used.
+ * @ac: The number of parameters passed to the executable file
  * @av: The name of the program.
+ *
  * Return: Always 0.
  */
 int main(__attribute__((unused)) int ac, char **av)
